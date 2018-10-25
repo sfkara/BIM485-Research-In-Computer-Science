d842903430490a597792cae45d1352722b5579e8
Fırat KARA <sfkara@anadolu.edu.tr>
2018-10-25 20:19:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace tutorial_no1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fotoName = "d:\\sfkara.jpg";
+            Image<Bgr, byte>  img = new Image<Bgr, byte>(fotoName);
+            CvInvoke.Imshow("Image", img);
+
+            CvInvoke.WaitKey(0);
+        }
+    }
+}
